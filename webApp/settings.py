dc944a3ee8e1a3c63e5004d44ee6d5468e3800ed
winsphinx <winsphinx@gmail.com>
2019-06-04 21:12:30 +0800

    update settings

--- webApp/settings.py
+++ webApp/settings.py
@@ -72,10 +72,20 @@ WSGI_APPLICATION = 'webApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'roam',  # 你的数据库名称
+        'USER': 'root',  # 你的数据库用户名
+        'PASSWORD': '',  # 你的数据库密码
+        'HOST': '',  # 你的数据库主机，留空默认为 localhost
+        'PORT': '3306',  # 你的数据库端口
     }
 }
 
