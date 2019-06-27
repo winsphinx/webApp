b342baee276d1d619cf04cb5e0bc993e78015ced
winsphinx <winsphinx@gmail.com>
2019-06-27 19:20:35 +0800

    set default date for SelectDateWidget

--- userApp/forms.py
+++ userApp/forms.py
@@ -1,11 +1,25 @@
 from django import forms
+from django.utils import timezone
 
 from .models import Orig
 
+MONTHS = {
+    1: ('一月'),
+    2: ('二月'),
+    3: ('三月'),
+    4: ('四月'),
+    5: ('五月'),
+    6: ('六月'),
+    7: ('七月'),
+    8: ('八月'),
+    9: ('九月'),
+    10: ('十月'),
+    11: ('十一月'),
+    12: ('十二月')
+}
 
-class OrigForm(forms.ModelForm):
-    class Meta:
-        model = Orig
-        fields = ['day']
-        labels = {'day': '选择日期'}
-        widgets = {'day': forms.SelectDateWidget()}
+
+class OrigForm(forms.Form):
+    day = forms.DateField(initial=timezone.now(),
+                          label='选择日期',
+                          widget=forms.SelectDateWidget(months=MONTHS))
